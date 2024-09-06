9b957d626e50d47ed8f6aac83010cbaf3680e8dd
Kenji Aranha Sato <30627646+KenjiSato97@users.noreply.github.com>
2024-09-06 16:36:30 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,40 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal é uma skatista brasileira, conhecida como Fadinha, que se tornou a mais jovem medalhista olímpica da história do Brasil. Com seu estilo único e carisma, ela conquistou o mundo do skate e inspirou uma nova geração de atletas",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024. Sua técnica apurada e determinação a tornaram uma das principais atletas do judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "Judô medalha de ouro "
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta artística brasileira que conquistou diversas medalhas em competições internacionais, incluindo os Jogos Olímpicos. Sua versatilidade e força a colocaram entre as melhores ginastas do mundo.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginastica medalha de ouro"
+    },
+    {
+        titulo: "Larissa Pimenta",
+        descricao: "Larissa Cincinato Pimenta é uma judoca e medalhista olímpica brasileira e bicampeã nos Jogos Pan-Americanos.Atualmente ela ocupa a 10ª posição no ranking mundial de judô e é Terceiro-Sargento RM2 - EP da Marinha do Brasil, onde é atleta do Programa de Alto Rendimento das Forças Armadas.",
+        link: "https://pt.wikipedia.org/wiki/Larissa_Pimenta",
+        tags: "Judô medalha de bronze"
+    },
+    {
+        titulo: "Gabriela Guimarães",
+        descricao : "Gabriela Guimarães é uma das principais atacantes da seleção brasileira de vôlei. Conhecida por seus ataques potentes e versatilidade, ela foi fundamental para a conquista de diversas medalhas para o Brasil.",
+        link : "https://pt.wikipedia.org/wiki/Gabriela_Guimarães",
+        tags: "Volei medalha de bronze"
+    },
+    {
+        titulo: "Debinha",
+        descricao : "Debinha é uma atacante de futebol brasileira, reconhecida por sua habilidade técnica e faro de gol. Ela é uma das principais jogadoras da seleção brasileira e de seu clube.",
+        link : "https://pt.wikipedia.org/wiki/Debinha",
+        tags: "futebol feminino medalha de bronze"
+    },
+    
+  ];
+
